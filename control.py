be27ebd0e719dfdcb9e9db2499842d85e7261387
manuela araújo <104527846+manuelafba@users.noreply.github.com>
2023-12-12 13:53:43 +0000

    Alterações no design

--- control.py
+++ control.py
@@ -1,5 +1,5 @@
 import flet as ft
-import tela1, tela2
+import tela1, tela2, tela3
 
 def init(p):
     global page, telas, cadastros        
@@ -8,6 +8,7 @@ def init(p):
     telas = {
         '0': tela1.view(),
         '1': tela2.view(),
+        '2': tela3.view(),
     }
     
 
@@ -18,16 +19,8 @@ def route_change(route):
     )          
     page.update()
 
-# def barra_navegacao():
-#     return ft.NavigationBar(
-#                         destinations=[
-#                             ft.NavigationDestination(icon=ft.icons.SAVE, label="Cadastrar"),
-#                             ft.NavigationDestination(icon=ft.icons.SEARCH, label="Pesquisar"),                            
-#                         ],
-#                         on_change= menu
-#             )#NavigationBar
-    
+
 
 def menu(e):
     tela2.components["tabela"].current.rows = tela2.data_table()
-    # page.go(str(e.control.selected_index))
+    
--- tela1.py
+++ tela1.py
@@ -38,11 +38,11 @@ def changetheme(e):
     c.page.theme_mode = "light" if c.page.theme_mode =="dark" else "dark"
     c.page.update()
  
-    c.time.sleep(0.5)
+    
  
     toggledarklight.selected = not toggledarklight.selected
  
-    c.page.splash.visible = False
+    
     c.page.update()
 
 toggledarklight = ft.IconButton(
@@ -50,7 +50,7 @@ toggledarklight = ft.IconButton(
     icon="dark_mode",
     selected_icon="light_mode",
     style=ft.ButtonStyle(
-    color={"":ft.colors.BLACK,"selected":ft.colors.WHITE}))
+    color={"":ft.colors.WHITE,"selected":ft.colors.WHITE}))
 
 
 # Funcao que mostre todos os componentes da tela 1
@@ -65,38 +65,41 @@ def view():
                 [                           
                     ft.Column(
                         [
-                            ft.Row([ft.Container( content=ft.Text("Cadastro", size=20))],alignment=ft.MainAxisAlignment.CENTER),
+                            ft.Row([ft.Container( content=ft.Text("Área de Preenchimento de Cadastro", size=20))],alignment=ft.MainAxisAlignment.CENTER),
                             ft.Column([
                                 ft.Row([image_holder],alignment=ft.MainAxisAlignment.CENTER),
                                 ft.Row([
                                 # Botão de upload da foto
                                 ft.Container(content= 
                                 ft.ElevatedButton(
-                                text="Escolher foto", icon="image", on_click=
+                                text="Escolher foto", style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=10), padding=10),icon="image", on_click=
                                 button_clicked
                                 ))
                                 ],alignment=ft.MainAxisAlignment.CENTER)#Container,
-                                ]),
+                                ]),    
                             ft.TextField(ref=components['tf_nome'], label="Nome", autofocus=True,prefix_icon=ft.icons.PERSON, helper_text="Apenas letras"),
                             ft.TextField(ref=components['tf_cpf'], label="CPF", prefix_icon=ft.icons.DOCUMENT_SCANNER, helper_text="xxx.xxx.xxx-xx"),
-                            ft.TextField(ref=components['tf_rg'], label="RG", prefix_icon=ft.icons.DOCUMENT_SCANNER, helper_text="xxxxxxx"),
+                            ft.TextField(ref=components['tf_rg'], label="RG", prefix_icon=ft.icons.DOCUMENT_SCANNER, helper_text="Deve conter 7 digitos"),
                             ft.TextField(ref=components['tf_telefone'], label="Telefone", prefix_icon=ft.icons.PHONE, helper_text="(xx) xxxxx-xxxx"),
-                            ft.TextField(ref=components['tf_endereço'], label="Endereço",prefix_icon=ft.icons.HOME),
+                            ft.TextField(ref=components['tf_endereço'], label="Endereço",prefix_icon=ft.icons.HOME, helper_text="Deve conter no máximo 20 caracteres"),
                             ft.TextField(ref=components['tf_nascimento'], label="Nascimento",prefix_icon=ft.icons.STAR, helper_text="DD/MM/AAAA"),
                             ft.TextField(ref=components['tf_e-mail'], label="E-mail",prefix_icon=ft.icons.EMAIL,helper_text="name@example.com ou name@example.com.br"),
                             ft.Row(
                                 [
                                     ft.Container(
                                         #Botao de cadastrar
-                                            content= ft.ElevatedButton(
-                                                        text="Cadastrar", 
+                                            content= ft.FilledButton(
+                                                        text="Cadastrar", style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=10), padding=10),
                                                         icon="save", 
                                                         on_click= lambda e: cadastrar(e, selected_file_path)
                                                     )#ElevatedButton   
                                     ),#Container
                                     ft.Container(
                                             content= ft.ElevatedButton(
-                                                        text="Pesquisar", 
+                                                        text="Pesquisar", style=ft.ButtonStyle(
+                                shape=ft.RoundedRectangleBorder(radius=10), padding=10), 
                                                         icon="search",
                                                         on_click= navigate_to_tela2
                                                     )#ElevatedButton   
@@ -109,17 +112,17 @@ def view():
                         expand=True,
                     ),                    
                 ],
-                # navigation_bar= c.barra_navegacao(),
+                
                 appbar= ft.AppBar(            
-                    title=ft.Text("Sistema de cadastro"),
-                    center_title=False,
-                    bgcolor=ft.colors.SURFACE_VARIANT,
+                    title=ft.Text("Sistema de cadastro", font_family="RobotoSlab",weight=ft.FontWeight.BOLD, color=ft.colors.WHITE),
+                    center_title=True,
+                    bgcolor=ft.colors.RED_900,
                     actions=[toggledarklight]                   
                 ),    
             )
     
 def cadastrar(e, selected_file_path = None):
-    print(selected_file_path)
+    
 
     nome = components['tf_nome'].current.value
     cpf = components['tf_cpf'].current.value
@@ -128,7 +131,8 @@ def cadastrar(e, selected_file_path = None):
     endereco = components['tf_endereço'].current.value
     nascimento = components['tf_nascimento'].current.value
     email = components['tf_e-mail'].current.value
-
+    import csv
+    import uuid
     components['tf_nome'].current.error_text = error_message('nome')
     components['tf_cpf'].current.error_text = error_message('cpf')
     components['tf_rg'].current.error_text = error_message('rg')
@@ -136,19 +140,43 @@ def cadastrar(e, selected_file_path = None):
     components['tf_endereço'].current.error_text = error_message('endereco')
     components['tf_nascimento'].current.error_text = error_message('nascimento')
     components['tf_e-mail'].current.error_text = error_message('email')
+    # exibir_snackbar_imagem_salva(selected_file_path)
     c.page.update()
 
-    # Se todos os campos forem válidos, escreva no arquivo CSV
+    # # Se todos os campos forem válidos, escreva no arquivo CSV
+    # if validar_nome(nome) and validar_telefone(telefone) and validar_cpf(cpf) and validar_rg(rg) and validar_email(email) and validar_nascimento(nascimento) and validar_endereco(endereco) and selected_file_path is not None:
+    #     dados = [
+    #         [nome, telefone, cpf, rg, endereco, nascimento, email, selected_file_path]]
+
+    #     # Abre o arquivo em modo de escrita
+    #     with open('bd.csv', 'a') as arquivo:
+    #         # arquivo.write('Nome,Telefone,CPF,RG,Endereco,Nascimento,E-mail,Upload de Foto\n')
+    #         for linha in dados:
+    #             arquivo.write(','.join(linha) + '\n')
     if validar_nome(nome) and validar_telefone(telefone) and validar_cpf(cpf) and validar_rg(rg) and validar_email(email) and validar_nascimento(nascimento) and validar_endereco(endereco) and selected_file_path is not None:
         dados = [
             [nome, telefone, cpf, rg, endereco, nascimento, email, selected_file_path]]
 
-        # Abre o arquivo em modo de escrita
-        with open('bd.csv', 'a') as arquivo:
-            # arquivo.write('Nome,Telefone,CPF,RG,Endereco,Nascimento,E-mail,Upload de Foto\n')
-            for linha in dados:
-                arquivo.write(','.join(linha) + '\n')
+        # Abre o arquivo CSV em modo de leitura para determinar o último ID usado
+        with open('bd.csv', 'r', newline='') as arquivo_leitura:
+            reader = csv.DictReader(arquivo_leitura)
+            ids_existentes = set([linha['id'] for linha in reader])
 
+        # Abre o arquivo CSV em modo de escrita e adiciona os novos dados com IDs
+        with open('bd.csv', 'a', newline='') as arquivo:
+            fieldnames = ['Nome', 'Telefone', 'CPF', 'RG', 'Endereco', 'Nascimento', 'E-mail', 'Upload de Foto','id']
+            writer = csv.DictWriter(arquivo, fieldnames=fieldnames)
+
+            # Cria um novo ID único para cada linha de dados e verifica se é único
+            for linha in dados:
+                novo_id = str(uuid.uuid4())
+                while novo_id in ids_existentes:  # Garante que o ID seja único
+                    novo_id = str(uuid.uuid4())
+                ids_existentes.add(novo_id)
+
+                # Cria um dicionário com os dados e o novo ID
+                linha_com_id = {'Nome': linha[0], 'Telefone': linha[1], 'CPF': linha[2], 'RG': linha[3], 'Endereco': linha[4], 'Nascimento': linha[5], 'E-mail': linha[6], 'Upload de Foto': linha[7],'id': novo_id,}
+                writer.writerow(linha_com_id)
         #Zera os TextFields
         components['tf_nome'].current.value = ''    
         components['tf_cpf'].current.value = ''
@@ -159,7 +187,24 @@ def cadastrar(e, selected_file_path = None):
         components['tf_e-mail'].current.value = ''
         selected_file_path = None
         image_holder.visible = False
+        c.page.snack_bar = ft.SnackBar(
+        ft.Text("Cadastro realizado com sucesso", size=20),
+        duration=2000,
+        bgcolor="green"
+        )
+        c.page.snack_bar.open = True
+        tela2.atualizar_csv([])
         c.page.update()
+
+# def exibir_snackbar_imagem_salva(selected_file_path):
+#     if selected_file_path is None:
+#         snack_bar = ft.SnackBar(
+#             ft.Text("Você deve escolher uma foto", size=20),
+#             duration=800,
+#             bgcolor="red"
+#         )
+#         c.snack_bar.open = True
+#     c.page.snack_bar = c.snack_bar
 
 def validar_nome(nome):
     # Verifica se o nome não contém números
@@ -192,7 +237,7 @@ def validar_nascimento(data):
 
 def validar_endereco(endereco):
     # Validação simples de endereço
-    return len(endereco) >= 5  # Exemplo: endereço deve ter pelo menos 5 caracteres
+    return 20 >= len(endereco) >= 5  # Exemplo: endereço deve ter pelo menos 5 caracteres
 
 def error_message(data):
     try:
@@ -228,10 +273,23 @@ def error_message(data):
                 raise ValueError("Por favor preencha o seu email.")
         elif data == 'nascimento':
             # Validação da data de nascimento
+            
+                    
             if not validar_nascimento(components['tf_nascimento'].current.value) and components['tf_nascimento'].current.value:
                 raise ValueError("Data de nascimento inválida(formato: DD/MM/AAAA)")
             elif not components['tf_nascimento'].current.value:
                 raise ValueError("Por favor preencha a sua data de nascimento.")
+            
+            if components['tf_nascimento'].current.value:
+                nascimento=components['tf_nascimento'].current.value
+                dia=int(nascimento[0]+nascimento[1])
+                mes=int(nascimento[3]+nascimento[4])
+                ano=int(nascimento[6]+nascimento[7]+nascimento[8]+nascimento[9])
+                
+                if not (0<dia<32  and 0<mes<13 and 1899<ano<2024):
+                    raise ValueError("Data de nascimento impossivel")
+                
+                
         elif data == 'endereco':
             if not validar_endereco(components['tf_endereço'].current.value) and components['tf_endereço'].current.value:
                 raise ValueError("Endereço inválido")
